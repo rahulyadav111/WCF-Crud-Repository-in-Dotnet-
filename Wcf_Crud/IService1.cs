e3f91f1fdb2e5427d49b4b959f53ce7404acae5c
Rahul Yadav <72229574+rahulyadav111@users.noreply.github.com>
2022-09-02 18:06:00 +0530

    Add files via upload

---
+++ Wcf_Crud/IService1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+
+namespace Wcf_Crud
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string InsertEmpDetails(EmpDetails eDatils);
+        [OperationContract]
+        DataSet GetEmpDetails(EmpDetails eDatils);
+        [OperationContract]
+        DataSet FetchUpdatedRecords(EmpDetails eDatils);
+        [OperationContract]
+        string UpdateEmpDetails(EmpDetails eDatils);
+        [OperationContract]
+        bool DeleteEmpDetails(EmpDetails eDatils);
+    }
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.  
+    [DataContract]
+    public class EmpDetails
+    {
+        int? eId;
+        string eName = string.Empty;
+        string eSalary = string.Empty;
+        string eDeptId = string.Empty;
+        string eDeptName = string.Empty;
+        [DataMember]
+        public int? Id
+        {
+            get
+            {
+                return eId;
+            }
+            set
+            {
+                eId = value;
+            }
+        }
+        [DataMember]
+        public string Name
+        {
+            get
+            {
+                return eName;
+            }
+            set
+            {
+                eName = value;
+            }
+        }
+        [DataMember]
+        public string Salary
+        {
+            get
+            {
+                return eSalary;
+            }
+            set
+            {
+                eSalary = value;
+            }
+        }
+        [DataMember]
+        public string DeptId
+        {
+            get
+            {
+                return eDeptId;
+            }
+            set
+            {
+                eDeptId = value;
+            }
+        }
+        [DataMember]
+        public string DeptName
+        {
+            get
+            {
+                return eDeptName;
+            }
+            set
+            {
+                eDeptName = value;
+            }
+        }
+    }
+}
---
+++ Wcf_Crud/Service1.svc.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Wcf_Crud
+{
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.  
+    public class Service1 : IService1
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString);
+        public string InsertEmpDetails(EmpDetails eDetails)
+        {
+            string Status;
+            SqlCommand cmd = new SqlCommand("USP_Emp_Insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", eDetails.Name);
+            cmd.Parameters.AddWithValue("@Salary", eDetails.Salary);
+            cmd.Parameters.AddWithValue("@DeptId", eDetails.DeptId);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Status = eDetails.Name + " " + eDetails.Salary + " registered successfully";
+            }
+            else
+            {
+                Status = eDetails.Name + " " + eDetails.Salary + " could not be registered";
+            }
+            con.Close();
+            return Status;
+        }
+        public DataSet GetEmpDetails(EmpDetails eDetails)
+        {
+            SqlCommand cmd = new SqlCommand("Get_AllEmployees", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.Id);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+        public DataSet FetchUpdatedRecords(EmpDetails eDetails)
+        {
+            SqlCommand cmd = new SqlCommand("Get_AllEmployees", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.DeptId);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+        public string UpdateEmpDetails(EmpDetails eDetails)
+        {
+            string Status;
+            SqlCommand cmd = new SqlCommand("USP_Emp_Update", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.Id);
+            cmd.Parameters.AddWithValue("@Name", eDetails.Name);
+            cmd.Parameters.AddWithValue("@Salary", eDetails.Salary);
+            cmd.Parameters.AddWithValue("@DeptId", eDetails.DeptId);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Status = "Record updated successfully";
+            }
+            else
+            {
+                Status = "Record could not be updated";
+            }
+            con.Close();
+            return Status;
+        }
+        public bool DeleteEmpDetails(EmpDetails eDetails)
+        {
+            SqlCommand cmd = new SqlCommand("USP_Emp_Delete", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", eDetails.DeptId);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return true;
+        }
+    }
+
+}
